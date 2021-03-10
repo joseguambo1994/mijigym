4667c9704720bb3d763e1846aa82dd25b176bb4b
jlguambo <jlguambo@gmail.com>
2021-03-09 21:29:56 -0500

    Add Trainee Frequency and Objective components, APIchanged to English

--- app.js
+++ app.js
@@ -6,12 +6,12 @@ const { query } = require("express");
 const cors = require("cors");
 var app = express();
 
-const trainee_level = [
+/* const trainee_level = [
   {id:1,trainee_level_type:"novato"},
   {id:2,trainee_level_type:"intermedio"},
   {id:3,trainee_level_type:"avanzadisimo"}
 ];
-
+ */
 const pool = new Pool({
     host: 'localhost',
     user: 'postgres',
@@ -21,22 +21,37 @@ const pool = new Pool({
 app.set('view engine','ejs');
 
 
-var query_result = {};
-var query_result_other= [];
+var query_result_level = {};
+var query_result_frequency = {};
+var query_result_objective = {};
 
 var query_trainee_level = 'select * from trainee_level;';
-const query_trainee_objective = 'select * from trainee_objective;';
-const query_training_frequency = 'select * from training_frequency;';
+var query_trainee_objective = 'select * from trainee_objective;';
+var query_training_frequency = 'select * from training_frequency;';
 
 pool.query(query_trainee_level,(err,res)=>{
     if (err) {
         console.log(err.stack)
       } else{
-        query_result = res.rows;
-        console.log(query_result);
+        query_result_level = res.rows;
       }
 });
 
+pool.query(query_trainee_objective,(err,res)=>{
+  if (err) {
+      console.log(err.stack)
+    } else{
+      query_result_objective = res.rows;
+    }
+});
+pool.query(query_training_frequency,(err,res)=>{
+  if (err) {
+      console.log(err.stack)
+    } else{
+      query_result_frequency = res.rows;
+      
+    }
+});
 
 
 app.use(cors());
@@ -46,14 +61,20 @@ app.get("/",(req,res,next)=>{
     console.log("Entraste")
   });
 
-app.get("/entrenamiento",(req,res)=>{
-  res.json(query_result);
-});
 
-app.get("/entrenamiento/nivel",(req,res)=>{
-  console.log(query_result);
-  res.json(query_result);
+app.get("/training/level",(req,res)=>{
+  res.json(query_result_level);
 })
+
+app.get("/training/frequency",(req,res)=>{
+  res.json(query_result_frequency);
+})
+
+app.get("/training/objective",(req,res)=>{
+  res.json(query_result_objective);
+})
+
+
 
 app.post("/user",(req,res)=>{
   res.json(req.body)
